eff771a3827be698afd1d15d8dbdedbf8d4ff827
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2026-06-11 15:25:00 +0200

    Fix road collision detection

--- cars/frist_car/main_car.gd
+++ cars/frist_car/main_car.gd
@@ -6,9 +6,11 @@ extends CharacterBody3D
 @export var deceleration: float = 24.0
 @export var steer_lerp_speed: float = 10.0
 @export var gravity: float = 9.8
+@export var ground_check_distance: float = 3.0
 
 var _current_speed: float = 0.0
 var _steer_input: float = 0.0
+var _ground_debug: String = "none"
 
 
 func _ready() -> void:
@@ -19,6 +21,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var throttle := Input.get_action_strength("movebackward") - Input.get_action_strength("moveforward")
 	var steer_target := Input.get_action_strength("moveleft") - Input.get_action_strength("moveright")
+	var has_road_below := _has_road_below()
 
 	if throttle != 0.0:
 		_current_speed = move_toward(_current_speed, throttle * move_speed, acceleration * delta)
@@ -29,13 +32,36 @@ func _physics_process(delta: float) -> void:
 
 	rotate_y(_steer_input * turn_speed * delta)
 
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	else:
+	if has_road_below and is_on_floor():
 		velocity.y = 0.0
+	else:
+		velocity.y -= gravity * delta
 
 	var forward := -global_transform.basis.z
 	velocity.x = forward.x * _current_speed
 	velocity.z = forward.z * _current_speed
 	move_and_slide()
-	print(is_on_floor())
+	print("is_on_floor=", is_on_floor(), " road_below=", has_road_below, " ground=", _ground_debug)
+
+
+func _has_road_below() -> bool:
+	var space_state := get_world_3d().direct_space_state
+	var from := global_position + Vector3.UP * 0.2
+	var to := global_position + Vector3.DOWN * ground_check_distance
+	var query := PhysicsRayQueryParameters3D.create(from, to)
+	query.exclude = [get_rid()]
+
+	var hit := space_state.intersect_ray(query)
+	if hit.is_empty():
+		_ground_debug = "none"
+		return false
+
+	var collider := hit["collider"] as Node
+	_ground_debug = "%s at %s" % [collider.get_path(), hit["position"]]
+
+	while collider != null:
+		if "Road" in collider.name:
+			return true
+		collider = collider.get_parent()
+
+	return false
